d92f1aea3a140bd245c9211d97d6e70c0f9459f6
dhiya <urrahmand0@gmail.com>
2026-04-04 17:53:29 +0700

    server actions redis setup

---
+++ app/actions.ts
@@ -0,0 +1,34 @@
+'use server'
+
+import { Redis } from "@upstash/redis";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+
+const redis = Redis.fromEnv();
+
+export async function createRoom(formData: FormData) {
+    const hostId = crypto.randomUUID();
+    // generate random 4-characterstring (DCAV)
+    const roomCode = Math.random().toString(36).substring(2, 6).toUpperCase();
+
+    const initialRoomState = {
+        status: 'waiting',
+        players: [
+            {
+                id: hostId,
+                name: 'PLAYER_1',
+                isHost: true,
+            }
+        ]
+    };
+
+    await redis.set(`room:${roomCode}`, initialRoomState);
+
+    (await cookies()).set('user_id', hostId, {
+        httpOnly: true,
+        path: '/',
+        sameSite: 'lax',
+    });
+
+    redirect(`room/${roomCode}`);
+}
