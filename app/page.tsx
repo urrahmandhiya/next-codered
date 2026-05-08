215a5fc3d123ed43c7ea823778dcb3eaf79b278a
veda <vedabezaleel@gmail.com>
2026-05-08 19:40:33 +0700

    style: eslint unused button components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import RoomEntry from "@/components/home/room-entry";
 import { Users } from "lucide-react";
-import { Button, buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 
