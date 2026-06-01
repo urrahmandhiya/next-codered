52278bb571aa471d077e7756fa8133d522c8a987
dhiya <urrahmand0@gmail.com>
2026-06-01 17:51:10 +0700

    chore: update the room mockup

--- lib/actions/mock.ts
+++ lib/actions/mock.ts
@@ -33,6 +33,7 @@ export async function goToWaitingRoom() {
         [`p:${hostId}:createdAt`]: hostState.createdAt,
         maxPlayersInRoom: 4,
         playersInRoom: 4,
+        phaseDuration: 15,
     };
 
     const rolesState: { [key: string]: string | number } = {}
