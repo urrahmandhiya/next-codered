dbede2f2dea2e5fed9e47cb19ef60514f7f5b46f
veda <veda3@fairtech.com.sg>
2026-07-04 16:07:18 +0800

    enh: set user_id cookie as non-httpOnly and add client-side document.cookie fallback

--- components/room/cookie-provider.tsx
+++ components/room/cookie-provider.tsx
@@ -1,15 +1,31 @@
 "use client"
 
-import { createContext, ReactNode, useContext } from "react";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 
 const CookieContext = createContext<string | null >(null)
 
 export default function CookieProvider({ cookieValue, children }: {cookieValue: string | null, children: ReactNode}) {
-    return(
-        <CookieContext.Provider value={cookieValue}>
+    const [value, setValue] = useState<string | null>(cookieValue);
+
+    useEffect(() => {
+        if (!value) {
+            const getCookie = (name: string) => {
+                const parts = `; ${document.cookie}`.split(`; ${name}=`);
+                if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
+                return null;
+            };
+            const val = getCookie("user_id");
+            if (val) {
+                setValue(val);
+            }
+        }
+    }, [value]);
+
+    return (
+        <CookieContext.Provider value={value}>
             {children}
         </CookieContext.Provider>
-    )
+    );
 }
 
 export function useUserCookies() {
